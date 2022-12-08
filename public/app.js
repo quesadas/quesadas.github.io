93d46f9f13241441bf5f712cc69f4fc6da05b778
quesadas <antonioquesada209@gmail.com>
2022-12-07 21:56:16 -0800

    changes

--- public/app.js
+++ public/app.js
@@ -2,6 +2,11 @@ import { app, auth, provider, db, onAuthStateChanged, signInWithPopup, signOut }
 
 const signInButton = document.getElementById('signInButton');
 const signOutButton = document.getElementById('signOutButton');
+const headerText = document.getElementById("introH");
+const headerP = document.getElementById("introP");
+const points = document.getElementById("listings");
+const pointsHeader = document.getElementById("listTitle");
+
 
 signInButton.onclick = () => signInWithPopup(auth, provider);
 signOutButton.onclick = () => {
@@ -18,10 +23,19 @@ onAuthStateChanged(auth, (user) => {
         signedInSection.hidden = false;
         signInButton.hidden = true;
         signOutButton.hidden = false;
+        headerText.hidden = true;
+        headerP.hidden = true;
+        pointsHeader.hidden = false;
+        points.hidden = false;
+
         userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3>`;
     } else {
         userDetails.innerHTML = '';
         signInButton.hidden = false;
         signOutButton.hidden = true;
+        headerText.hidden = false;
+        headerP.hidden = false;
+        pointsHeader.hidden = true;
+        points.hidden = true;
     }
 });
